57e2c7b6f5398ed8f810629d9900f9f75a802dac
Derzsi Dezso~ <derzsidezso@gmail.com>
2024-02-22 21:20:43 +0200

    components, newSet, logic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useState } from "react";
+import menu from "./data.js";
+import Title from "./Title.jsx";
+import Menu from "./Menu.jsx";
+import Categories from "./Categories.jsx";
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItem = menu.filter((item) => item.category === category);
+    setMenuItems(newItem);
+  };
+  return (
+    <main>
+      <Title text={"Our Menu"} />
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu items={menuItems} />
+    </main>
+  );
 };
 export default App;
